b25ef71b2ced3af5666fc6dfd2886eb238804ae7
Saurav <sauravjyotikalita56@gmail.com>
2025-01-25 23:28:38 +0530

    commit

--- .vscode/arrays.cpp
+++ .vscode/arrays.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 int main(){
     int arr[6];
+    cout<<"Enter the elements of the array  :"<<endl;
+    for(int i=0;i<6;i++){
+        cin>>arr[i];
+    }
+    float total=arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5];
+    cout<<"The sum of the elements of the array is :"<<total<<endl;
     
     return 0;
 }
